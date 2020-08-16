f775d6f69c2e82c82433c2165fa297279c06d263
markey <1053667066@qq.com>
2020-08-16 21:43:16 +0800

    add h2 utils demo

--- h2db/src/main/java/com/markey/h2/H2SpringbootApplication.java
+++ h2db/src/main/java/com/markey/h2/H2SpringbootApplication.java
@@ -1,5 +1,8 @@
 package com.markey.h2;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @ProjectName: MyUtils
  * @Package: com.markey.h2
