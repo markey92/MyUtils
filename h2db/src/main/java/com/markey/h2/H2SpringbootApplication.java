76d75e5fbde2971a49b7194d0fafa03b36b57944
markey <1053667066@qq.com>
2020-08-16 21:34:12 +0800

    init

---
+++ h2db/src/main/java/com/markey/h2/H2SpringbootApplication.java
@@ -0,0 +1,17 @@
+package com.markey.h2;
+
+/**
+ * @ProjectName: MyUtils
+ * @Package: com.markey.h2
+ * @ClassName: H2SpringbootApplication
+ * @Author: markey
+ * @Description:
+ * @Date: 2020/8/16 21:29
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class H2SpringbootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(H2SpringbootApplication.class, args);
+    }
+}
